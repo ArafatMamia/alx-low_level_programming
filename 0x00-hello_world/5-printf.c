b6b3a311b9280d634984c57d6f9f002ccc0c4693
Arafat Mamia <arafmamia@gmail.com>
2021-08-26 06:08:56 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * @void: no parameters
+ * description: uses the printf function
+ * Return: always 0(success)
+ */
+int main(void)
+{
+char *alxstr = "with proper grammar, but the outcome is the piece of art,";
+printf("%s\n", alxstr);
+return (0);
+}
