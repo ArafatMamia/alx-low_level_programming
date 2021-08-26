b468bf420aaa644c7d271a0248aaac4c26855c4f
Arafat Mamia <arafmamia@gmail.com>
2021-08-26 05:58:58 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * @void: no parameters
+ * Description: uses puts to output
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+char *alxstr = "\"Programming is like building a multilingual puzzle";
+
+puts(alxstr);
+return (0);
+}
